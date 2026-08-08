102d12750e970a7e8f278145beb427b3e78006ee
DidoNikolovV <didonikolov01@gmail.com>
2026-08-08 14:13:11 +0300

    Initialize repository

---
+++ src/main/java/com/dormify/dormify_api/DormifyApiApplication.java
@@ -0,0 +1,13 @@
+package com.dormify.dormify_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DormifyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DormifyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dormify/dormify_api/DormifyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dormify.dormify_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DormifyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
